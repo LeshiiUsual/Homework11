151ff284c497f6e7f5393b47cfa157816cc0bda3
Nikita Malakhov <i_am_malahov@mail.ru>
2021-11-10 11:51:51 +0300

    Add project files.

---
+++ Homework11.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+// Вызывает фуункцию std::cout
+void print()
+{
+    //Потому что хотим использовать стандартную Библиотеку для печати в консоль и 
+    //нехотим использовать другие Библиотеки
+    std::cout << "HelloSkiilBox\n";
+}
+
+/*
+Присваиваем значение переменных и печатаем HelloWorld
+*/
+int main()
+{   int x = 100;
+    3 + 7;
+    int y = x + 100;
+
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+    int mult = x * y;
+    int random=1002;
+
+    std::cout << random;
+}
